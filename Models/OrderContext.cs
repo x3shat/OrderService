d972ea316ee1db8d09a365cb2b804b6878fc4408
Shatantra Maji <55249027+x3shat@users.noreply.github.com>
2024-07-03 21:20:45 +0530

    Create OrderContext.cs

---
+++ Models/OrderContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderService.Models
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
+//do accordingly 
